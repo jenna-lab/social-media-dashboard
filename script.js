fd3089f5c3232ed0d6000ffcc2f005dc64bcd106
jenna <jensam209@gmail.com>
2024-01-12 09:45:44 +0300

    css html and script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// const slider = document.querySelector(".toggle-button");
+
+// slider.addEventListener("click", ()=>{
+//     document.body.classList.toggle("dark-theme");
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let toggleSwitch = document.querySelector(".switch");
+  let topBg = document.querySelector('.top-bg');
+	let body = document.querySelector('.body');
+	let card = document.querySelectorAll('.card');
+	let grayText = document.querySelectorAll('.gray-text');
+	
+  
+
+  toggleSwitch.addEventListener("change", () => {
+    body.classList.toggle('body-light');
+    topBg.classList.toggle('top-bg-light');
+		
+		card.forEach(card => {
+			card.classList.toggle('card-light');
+			if(!card.classList.contains('dark')) {
+				card.classList.add('dark');
+			} else {
+				card.classList.remove('dark');
+			}
+		})
+
+		grayText.forEach(gText => {
+			gText.classList.toggle('gray-text-light')
+		})
+  });
+});
